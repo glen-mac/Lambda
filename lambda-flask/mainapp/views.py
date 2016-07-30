728e3d5c8787a514c8e03ecb3efb079f56f189e5
Andi <andi.winata@hotmail.com>
2016-07-30 14:14:23 +1000

    Trying to integrate pred stuff with backend

--- lambda-flask/mainapp/views.py
+++ lambda-flask/mainapp/views.py
@@ -2,7 +2,14 @@ from mainapp import mainapp, api
 from flask import render_template, flash, redirect, url_for, request, jsonify
 import forms
 from flask_restful import reqparse, abort, Api, Resource
+import numpy as np
+import sys
+
+sys.path.insert(0, 'D:\GovHackGitHub\predstuff')
+print(sys.path)
 import predstuff
+
+from predstuff import get_model
 
 @mainapp.route('/')
 def index():
@@ -14,7 +21,10 @@ parser.add_argument('userdata')
 
 class TaxProcess(Resource):
     def get(self):
-        return 'there is nothing in here, use post'
+        # return 'there is nothing in here, use post'
+        mdl = get_model.load_model\
+            ('D:\\GovHackGitHub\\predstuff\\models\\reg_baseline.p')
+        get_model.predict(mdl, np.random.random_sample((35,)))
 
     def post(self):
         args = parser.parse_args()
